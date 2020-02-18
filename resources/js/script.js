216ccf1d84a28cb6a17c848643d7700f895e90a3
Kamil Adamus <44910472+prajns@users.noreply.github.com>
2020-02-18 18:19:07 +0100

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+
+    $('.js--scroll-header').click(function () {
+        $('html, body').animate({scrollTop: $('header').offset().top}, 1000); 
+    });
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+    });
+
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('fa-bars')) {
+            icon.addClass('fa-angle-up');
+            icon.removeClass('fa-bars');
+        } else {
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-angle-up');
+        }        
+    });
+
+});
